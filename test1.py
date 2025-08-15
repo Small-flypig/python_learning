3b49f3fe0e012238e8c17b5bcd1ca6a2a9078ab4
longtourister <2052746464@qq.com>
2025-08-15 10:33:26 +0800

    enter

--- test1.py
+++ test1.py
@@ -1,1 +1,27 @@
-print("hello world")
+"""getattr()"""
+class Test(object):
+    x = "nihao"
+a = Test()
+print(getattr(a, 'x'))  # 获取属性 x 值
+print(getattr(a, 'y', 'None'))  # 获取属性 y 值不存在，但设置了默认值
+# print(getattr(a, 'y'))    # AttributeError: 'Test' object has no attribute 'y'
+print(a.x)  # 效果等同于上面
+
+"""__next__()"""
+string="hello"
+ite=iter(string)
+print(ite.__next__())#从0开始
+print(ite.__iter__())#返回迭代器本身，类似指针
+#print(ite(1))#错误用法
+
+for i in string: 
+    print(i)
+
+for i in iter(string):
+    print(i)
+
+"""切片"""
+lst = [10,20,30,40,50,60,70,80]
+#切片为：start=2 , stop=6 , step=1
+lst2 = lst[2:6:2]
+print(lst2)    #[30, 40, 50, 60]
